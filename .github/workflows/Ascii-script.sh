3056efd33700575d4489341183051864023ed15d
Eman-Shehta <emanshshta@gmail.com>
2025-04-22 16:20:14 +0200

    add execute perm

--- .github/workflows/Ascii-script.sh
+++ .github/workflows/Ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
-            sudo apt-get install cowsay -y
-            cowsay -f dragon "run for cover, i am a DRAGON " >>dragon.txt
-            grep -i "dragon" dragon.txt
-            cat dragon.txt
-            ls -ltra
+sudo apt-get install cowsay -y
+cowsay -f dragon "run for cover, i am a DRAGON " >>dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
